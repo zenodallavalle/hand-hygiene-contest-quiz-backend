61794487b5c5f1f91bcb9b9be1384d574b3677c8
Zeno Dalla Valle <zenodallavalle@gmail.com>
2023-06-27 23:10:31 +0200

    bug fix in wsgi

--- hand_hygiene_contest_backend/wsgi.py
+++ hand_hygiene_contest_backend/wsgi.py
@@ -8,9 +8,28 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import time
+import traceback
+import signal
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hand_hygiene_contest_backend.settings")
+sys.path.append("/var/www/vhosts/hand-hygiene-contest-backend")
+# adjust the Python version in the line below as needed
+sys.path.append(
+    "/var/www/vhosts/hand-hygiene-contest-backend/env/lib/python3.8/site-packages"
+)
 
-application = get_wsgi_application()
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "hand_hygiene_contest_backend.production_settings"
+)
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Error loading applications
+    if "mod_wsgi" in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
