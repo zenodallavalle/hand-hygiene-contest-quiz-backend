f885d4edb1cbb081f2f7bbd0d49e2752df6e9d9f
Zeno Dalla Valle <zenodallavalle@gmail.com>
2023-06-27 23:14:11 +0200

    updated scripts

--- scripts/redeploy-production.sh
+++ scripts/redeploy-production.sh
@@ -2,7 +2,7 @@
 
 git pull origin main
 
-python manage.py migrate
-python manage.py collectstatic --noinput
+python manage.py migrate --settengs "hand_hygiene_contest_quiz.production_settings"
+python manage.py collectstatic --noinput  --settengs "hand_hygiene_contest_quiz.production_settings"
 
 sudo apache2ctl restart
