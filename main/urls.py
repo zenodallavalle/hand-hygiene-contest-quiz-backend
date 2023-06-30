189de33cc64ed67101f26a3e5d21e76fb5678443
Zeno Dalla Valle <zenodallavalle@gmail.com>
2023-06-30 16:32:15 +0200

    added redirect from / to /admin/

--- main/urls.py
+++ main/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
-    path("", include("main.urls")),
-    path("admin/", admin.site.urls),
-    path("api/v1/", include("api_v1.urls")),
+    path("", lambda request: HttpResponseRedirect("/admin/"), name="home"),
 ]
