f1ee9138a30779730c2c0a246d2360d8c6234fd5
Zeno Dalla Valle <zenodallavalle@gmail.com>
2023-06-30 15:26:12 +0200

    changed redeploy hook

--- hooks/redeploy-production.sh
+++ hooks/redeploy-production.sh
@@ -3,10 +3,10 @@
 
 
 cd /var/www/vhosts/hand-hygiene-contest-quiz-backend
-. env/bin/activate
 
-ssh-keyscan github.com >> ~/.ssh/known_hosts
 git pull origin main
+
+. env/bin/activate
 
 python manage.py migrate --settings "hand_hygiene_contest_backend.production_settings"
 python manage.py collectstatic --noinput  --settings "hand_hygiene_contest_backend.production_settings"
